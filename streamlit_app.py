38d02b241be65cebfec62621eaaccbc698be689e
NickSzydlowski <nick.szydlowski@sjsu.edu>
2024-10-28 13:10:41 -0700

    add text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,14 @@ st.title("Return Multiple Responses")
 
 with st.form("my_form"):
     st.write("Inside the form")
-    slider_val = st.slider("Form slider", max_value=1)
+    slider_val = st.slider("Temperature", max_value=1, step=.1)
+    text_val = st.text_area("Query")
     checkbox_val = st.checkbox("Form checkbox")
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
+        st.write("slider", slider_val, "query", text_val, "checkbox", checkbox_val)
 st.write("Outside the form")
 
 
