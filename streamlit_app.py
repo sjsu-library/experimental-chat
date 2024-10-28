8148b62aeeeeab5627b552fa52fc08a2f78a21c8
NickSzydlowski <nick.szydlowski@sjsu.edu>
2024-10-28 16:23:49 -0700

    add range to parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.title("Return Multiple Responses")
 
 with st.form("my_form"):
     st.write("Inside the form")
-    temp_val = st.slider("Temperature", min_value=0.0, max_value=1.0, step=.1)
-    topk_val = st.slider("Tokens Considered", min_value=1, max_value=10, step=1)
+    temp_val = st.slider("Temperature", min_value=0.0, max_value=2.0, step=.1)
+    topk_val = st.slider("Tokens Considered", min_value=1, max_value=100, step=1)
     text_val = st.text_area("Query")
 
     # Every form must have a submit button.
