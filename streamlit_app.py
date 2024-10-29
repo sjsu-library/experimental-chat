fe362b048d8b60ecb06efde10d038537c37417af
NickSzydlowski <nick.szydlowski@sjsu.edu>
2024-10-28 18:45:18 -0700

    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,11 @@ with st.form("my_form"):
         #llm = Gemini(model="models/gemini-1.5-flash", api_key=st.secrets.google_gemini_key, temperature=temp_val, topK = topk_val, topP = 1.0)
         model = genai.GenerativeModel(
             'gemini-1.5-flash',
-            generation_config=genai.GenerationConfig(
-            max_output_tokens=2000,
-            temperature=temp_val,
-            "topK"=topk_val,
-            "topP"=topp_val,
+            generation_config=(
+            "max_output_tokens":2000,
+            "temperature":temp_val,
+            "topK":topk_val,
+            "topP":topp_val,
         ))
         i = 0
         while i < 11:
