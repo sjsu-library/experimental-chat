05bdced4bf777f5f6e0c3da5efebe58e3a52d889
NickSzydlowski <nick.szydlowski@sjsu.edu>
2024-10-29 12:26:16 -0700

    Add comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,17 @@
 import streamlit as st
-#from llama_index.llms.gemini import Gemini
-#from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-#from llama_index.embeddings.gemini import GeminiEmbedding
 import google.generativeai as genai
 
+#Get API Key
 GOOGLE_API_KEY= st.secrets.google_gemini_key
 genai.configure(api_key=GOOGLE_API_KEY)
 
-st.set_page_config(page_title="Return Multiple Responses", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Return Multiple Responses", page_icon="🐇", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Return Multiple Responses")
 
 
-
+#create a form
 with st.form("my_form"):
-    st.write("Inside the form")
+    #form controls for temperature, topK, topP, and query
     temp_val = st.slider("Temperature", min_value=0.0, max_value=2.0, step=.1)
     topk_val = st.slider("Tokens Considered", min_value=1, max_value=100, step=1)
     topp_val = st.slider("Top P", min_value=0.0, max_value=1.0, step=0.05)
@@ -22,20 +20,20 @@ with st.form("my_form"):
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        #st.write("temperature", temp_val, "query", text_val, "tokens considered", topk_val) 
-        #llm = Gemini(model="models/gemini-1.5-flash", api_key=st.secrets.google_gemini_key, temperature=temp_val, topK = topk_val, topP = 1.0)
+        # set LLM parameters
         model = genai.GenerativeModel(
             'gemini-1.5-flash',
             generation_config={'max_output_tokens':100,'temperature':temp_val,'top_k':topk_val,'top_p':topp_val }
         )
         i = 0
-        while i < 11:
-            #resp = llm.complete(text_val)
+        #loop 10 times
+        while i < 10:
+            #get response from LLM
             resp = model.generate_content(text_val)
+            #write response to the page - need to add error catching here for harmful content
             st.write(resp.text)
             st.divider()
             i +=1
 
-st.write("Outside the form")
 
 
