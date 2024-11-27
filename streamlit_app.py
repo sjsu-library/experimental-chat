ce84b14ba4784c98a49fe34ab0f7e4ae8a3f54df
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-11-27 10:54:57 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 st.set_page_config(page_title="10x Chatbot", page_icon="🐇", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("10x Chatbot")
-st.info("This application queries Google Gemini ten times for each prompt. This can be helpful in demonstrating the effects of temperature and other parameters that control randomness."
+st.write("This application queries Google Gemini ten times for each prompt. This can be helpful in demonstrating the effects of temperature and other parameters that control randomness.")
 
 #create a form
 with st.form("my_form"):
