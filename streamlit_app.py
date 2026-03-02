86b466d1db9ca39d159fb87fa68ffdc619a7b008
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2026-03-02 10:15:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ GOOGLE_API_KEY= st.secrets.google_gemini_key
 genai.configure(api_key=GOOGLE_API_KEY)
 
 st.set_page_config(page_title="AI Bias Explorer", page_icon="🔍", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.image("dhc_banner.png", width=300)
 st.title("AI Bias Explorer")
 st.write("This application queries Google Gemini ten times for each prompt. This can be helpful in demonstrating the effects of temperature and other parameters that control randomness. The different controls interact with one another - try moving all three all the way to the right to see the most randomness.")
 st.write("&larr; Less Random ---- More Random &rarr;")
